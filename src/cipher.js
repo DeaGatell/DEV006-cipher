b015d03e45685fdf2af6acd037cdb0f5a305623f
Dea Gatell <deagatell6@gmail.com>
2023-03-29 06:26:19 -0600

    third sprint

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,41 @@
 const cipher = {
-  // ...
+  encode: function(offset, string) {
+    if (typeof Number(offset) !== "number" || typeof string !== "string") {
+      throw new TypeError("The arguments must be a string and a number.");
+    }
+    let result = "";
+    for (let i=0; i<string.length; i++) {
+      const letterASCII = string[i].charCodeAt();
+      if (letterASCII >= 65 && letterASCII <= 90) {
+        result += String.fromCharCode(((letterASCII - 65 + Number(offset))%26)+65);
+      } else if (letterASCII >= 97 && letterASCII <= 122) {
+        result += String.fromCharCode(((letterASCII - 97 + Number(offset))%26)+97);
+      } else {
+        result += string[i];
+      }
+    }
+    return result;
+  },
+  decode: function(offset, string) {
+    if (typeof Number(offset) !== "number" || typeof string !== "string") {
+      throw new TypeError("The arguments must be a string and a number.");
+    }
+    while (offset > 26) {
+      offset -= 26;
+    }
+    let result = "";
+    for (let i=0; i<string.length; i++) {
+      const letterASCII = string[i].charCodeAt();
+      if (letterASCII >= 65 && letterASCII <= 90) {
+        result += String.fromCharCode(((letterASCII - 65 - Number(offset) + 26)%26)+65);
+      } else if (letterASCII >= 97 && letterASCII <= 122) {
+        result += String.fromCharCode(((letterASCII - 97 - Number(offset) + 26)%26)+97);
+      } else {
+        result += string[i];
+      }
+    }
+    return result;
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
+/* eslint-disable no-console */
 import cipher from './cipher.js';
 
 console.log(cipher);
 
-const begin = document.getElementById("begin");
-begin.addEventListener("click", () => {
-    console.log("click");
+//Cipher Section//
+
+const cipherOffset1 = document.getElementById("thisIsN1");
+cipherOffset1.addEventListener("input", () => {
+  const string = document.getElementById('ciphertext').value;
+  const offset = document.getElementById('thisIsN1').value;
+  const resultSpan = document.getElementById('result1');
+
+  resultSpan.textContent = cipher.encode (offset, string);
+});
+
+//Decipher Section//
+
+const cipherOffset2 = document.getElementById("thisIsN2");
+cipherOffset2.addEventListener("input", () => {
+  const string = document.getElementById('deciphertext').value;
+  const offset = document.getElementById('thisIsN2').value;
+  const resultSpan = document.getElementById('result2');
+
+  resultSpan.textContent = cipher.decode (offset, string);
+});
+
+//Buttons//
+
+const info = document.getElementById ('instructions');
+info.addEventListener ("click", () => {
+  console.log('click');
+  alert("To cipher: Write your message in the first text area, choose an offset by scrolling through the bar, copy the message and paste wherever you'd like to use it. \n\nTo decipher: Paste your secret message in the second text area, choose the same offset by scrolling through the bar and uveil the secret message.")
+});
+
+const sugg = document.getElementById ('suggestions');
+sugg.addEventListener ("click", () => {
+  console.log('click');
+  alert("You can use this page to cipher and decipher secret messages with friends and family. \nHowever if you are in danger and in real need of help, we suggest you contact any of these emergency numbers (Mexico): \n\n911 - Emergency \n089 - Anonymous report \n088 - Federal Police \n*765 - Women SOS Line")
 });
